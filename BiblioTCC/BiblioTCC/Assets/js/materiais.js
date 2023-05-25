7f8da42601676239dcc0b10d4917ef211342b2b7
JulianaSabs <116669209+JulianaSabs@users.noreply.github.com>
2023-05-24 21:58:27 -0300

    Add files via upload

---
+++ BiblioTCC/BiblioTCC/Assets/js/materiais.js
@@ -0,0 +1,24 @@
+﻿function AbrirModal(titulo, mensagem) {
+    document.getElementById("modal").classList.add("active");
+    document.getElementById("tituloModal").innerText = titulo;
+    document.getElementById("mensagemModal").innerText = mensagem;
+}
+function FecharModal() {
+    document.getElementById("modal").classList.remove("active");
+}
+
+window.onload = function () {
+    AlterarCorTituloDaPagina();
+    AlterarCorMenuLateral();
+}
+
+function AlterarCorTituloDaPagina() {
+    let tituloDaPagina = document.querySelector("#form1 > nav > div.nav-list > ul > li:nth-child(4) > a");
+    tituloDaPagina.style.color = "#FFD369";
+    tituloDaPagina.style.borderBottom = "3px solid #FFD369";
+}
+
+function AlterarCorMenuLateral() {
+    let menuLateral = document.querySelector("div.area > nav > ul > li:nth-child(4) > a");
+    menuLateral.style.color = "#FFD369";
+}
