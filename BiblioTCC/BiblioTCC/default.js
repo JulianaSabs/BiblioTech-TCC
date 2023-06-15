69adbe4a076a0049bd3bac075af4b528ee90c7f4
JulianaSabs <116669209+JulianaSabs@users.noreply.github.com>
2023-06-15 15:39:21 -0300

    Delete default.js

--- BiblioTCC/BiblioTCC/default.js
+++
@@ -1,8 +0,0 @@
-﻿function AbrirModal(titulo, mensagem) {
-    document.getElementById("modal").classList.add("active");
-    document.getElementById("tituloModal").innerText = titulo;
-    document.getElementById("mensagemModal").innerText = mensagem;
-}
-function FecharModal() {
-    document.getElementById("modal").classList.remove("active");
-}
